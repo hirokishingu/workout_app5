9e4e568f0278f322749f117ff0366246f7cd8a31
HirokiSingu <hrk.bass.526@gmail.com>
2016-11-06 03:11:42 +0000

    wow!

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-         has_many :exercise
+         has_many :exercises
          
          def full_name
            [first_name, last_name].join(" ")
