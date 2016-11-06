5ba1a15f96a1d09ab0ec772e7d78d899afeab42f
HirokiSingu <hrk.bass.526@gmail.com>
2016-11-06 03:23:15 +0000

    修正

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,11 +9,11 @@ class ExercisesController < ApplicationController
    end
    
    def new
-       @exercise = current_user.exercise.new
+       @exercise = current_user.exercises.new
    end
    
    def create
-      @exercise = current_user.exercise.new(exercise_params)
+      @exercise = current_user.exercises.new(exercise_params)
       
       if @exercise.save
          flash[:notice] = "Exercise has been created"
